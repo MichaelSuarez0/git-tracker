be4d523229f20778cd222b547211a5757c685df7
MichaelSuarez0 <a20180264@pucp.edu.pe>
2026-09-17 19:25:18 -0500

    chore: actualiza versión en init.py y pyproject.toml

--- src/git_tracker/__init__.py
+++ src/git_tracker/__init__.py
@@ -2,3 +2,5 @@ from .commit_tracker import GitHubCommitsTracker
 from .repo_search import GitHubRepositorySearch
 
 __all__ = ["GitHubCommitsTracker", "GitHubRepositorySearch"]
+
+__version__ = "0.0.0"
